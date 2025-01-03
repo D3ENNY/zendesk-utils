08cd758d2335ffeeda07c7fbca6ffb95b2576d57
Denys Raimondi <denysraimondi06@gmail.com>
2025-01-03 12:46:04 +0100

    clean code removing some useless lines

--- background.js
+++ background.js
@@ -5,10 +5,9 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
       target: { tabId: tabId },
       function: () => {
         function makeAlert() {
-          document.body.style.backgroundColor = 'lightblue'; // Change to your desired color
           const elements = document.querySelectorAll(".sc-15mtwvo-0.gjlqHm");
           elements.forEach(element => {
-            if (element.textContent === "Pianificato") { // Use strict equality check
+            if (element.textContent === "Pianificato") { 
               element.style.backgroundColor = "#bD42ff4d";
             }
           });
