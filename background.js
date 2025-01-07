5996ee485977694b82c793a02405968b372fd836
Denys Raimondi <denysraimondi06@gmail.com>
2025-01-07 10:10:30 +0100

    cercando di icrementare la suddivisione del codice. senza successo

--- background.js
+++ background.js
@@ -1,19 +1,26 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status === 'complete') {
-    console.log("Tab updated, executing script...");
+    console.log("Tab updated, executing script...")
+
     chrome.scripting.executeScript({
       target: { tabId: tabId },
-      function: () => {
-        function makeAlert() {
-          const elements = document.querySelectorAll(".sc-15mtwvo-0.gjlqHm");
-          elements.forEach(element => {
-            if (element.textContent === "Pianificato") { 
-              element.style.backgroundColor = "#421c6b";
-            }
-          });
-        };
-        setInterval(makeAlert, 2000);
-      }
-    });
+      function: main()
+    })
   }
-});
+})
+
+function main(){
+  const ChangeColor = (content, color) => {
+    const elements = document.querySelectorAll(".sc-15mtwvo-0.gjlqHm");
+    elements.forEach(element => {
+      if (element.textContent === content) { 
+        element.style.backgroundColor = color
+      }
+    })
+  }
+
+  setInterval( () => {
+    ChangeColor("Pianificato", "#421c6b")
+  }, 2000)
+
+}
