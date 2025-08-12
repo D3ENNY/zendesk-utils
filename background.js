9c40e8f8ada07acfae0642382e6c571f80b0095d
jacopops <jacopo.conti@sinetinformatica.it>
2025-08-12 15:15:09 +0200

    Chore: add observer instead of timer to load the extension and rewrite the function ChangeElementColor for better performance

--- background.js
+++ background.js
@@ -12,16 +12,29 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 function main() {
 
 
-  const ChangeElementColor = (element, content, color) => {
+  const ChangeElementColor = (element, mapColor) => {
     let elements = [...document.querySelectorAll(element)]
 
-    elements.filter(el => el.textContent.trim() == content).forEach(el => {
-      el.style.backgroundColor = color
+    console.log("elements", elements)
+    elements.forEach(single => {
+      mapColor.forEach(el => {
+        console.log(single.textContent == el[0], single.textContent, el[0], el[1])
+        if (single.textContent == el[0]) {
+          single.style.backgroundColor = el[1]
+        }
 
-    })
-  } 
+      });
+    });
 
-  const UnderlineElement = (element,content, color) => {
+
+
+    // elements.filter(el => el.textContent.trim() == content).forEach(el => {
+    //   el.style.backgroundColor = color
+
+    // })
+  }
+
+  const UnderlineElement = (element, content, color) => {
     let elements = [...document.querySelectorAll(element)]
 
     elements.filter(el => el.textContent.includes(content)).forEach(el => {
@@ -31,17 +44,38 @@ function main() {
       el.style.textDecorationThickness = "2px"
     })
   }
-  
 
-  setInterval(() => {
-    ChangeElementColor(".sc-15mtwvo-0", "Pianificato", "#421c6b")
-    ChangeElementColor(".sc-15mtwvo-0", "In sospeso", "#00294aff")
-    ChangeElementColor(".sc-15mtwvo-0", "In lavorazione", "#4bb213ab")
-    ChangeElementColor(".sc-15mtwvo-0", "Aperto - 2Liv", "#008f39ab")
+
+  waitForElm('.sc-15mtwvo-0').then(() => {
+    console.log('Element is loaded');
+
+    let mapColor = [["Pianificato", "#421c6b"], ["In sospeso", "#00294aff"], ["In lavorazione", "#4bb213ab"], ["Aperto - 2Liv", "#008f39ab"]]
+    ChangeElementColor(".sc-15mtwvo-0", mapColor)
 
     UnderlineElement(".StyledButton-sc-qe3ace-0", "[Failed]", "#ff0000")
     UnderlineElement(".StyledButton-sc-qe3ace-0", "[Warning]", "#ffa500")
     UnderlineElement(".StyledButton-sc-qe3ace-0", "[Success]", "#00ff00")
-  }, 2000)
+  });
+
+
+  function waitForElm(selector) {
+    return new Promise(resolve => {
+      if (document.querySelector(selector)) {
+        return resolve(document.querySelector(selector));
+      }
+
+      const observer = new MutationObserver(mutations => {
+        if (document.querySelector(selector)) {
+          observer.disconnect();
+          resolve(document.querySelector(selector));
+        }
+      });
+
+      observer.observe(document.body, {
+        childList: true,
+        subtree: true
+      });
+    });
+  }
 
 }
