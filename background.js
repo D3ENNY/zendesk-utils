b7cafca801a4b9376ebc1ccb8bbdc748b6ce5625
RaimondiDenys <denys.raimondui@sinetinformatica.it>
2025-09-15 08:35:52 +0200

    Refactoring behavior code

--- background.js
+++ background.js
@@ -16,35 +16,22 @@ function main() {
 
   const ChangeElementColor = (element, mapColor) => {
     let elements = [...document.querySelectorAll(element)]
-    console.log("call color")
-    // console.log("elements", elements)
+    console.log("call ChangeElementColor")
+
     elements.forEach(single => {
       mapColor.forEach(el => {
-        // console.log(single.textContent == el[0], single.textContent, el[0], el[1])
         if (single.textContent == el[0]) {
           single.style.backgroundColor = el[1]
         }
 
-      });
-    });
+      })
+    })
 
   }
 
-  document.onclick = function (event) {
-    waitForElm('.sc-15mtwvo-0').then(() => {
-      console.log('Element is loaded');
-
-      ChangeElementColor(".sc-15mtwvo-0", mapColor)
-
-      UnderlineElement(".StyledButton-sc-qe3ace-0", "[Failed]", "#ff0000")
-      UnderlineElement(".StyledButton-sc-qe3ace-0", "[Warning]", "#ffa500")
-      UnderlineElement(".StyledButton-sc-qe3ace-0", "[Success]", "#00ff00")
-
-    });
-  };
-
   const UnderlineElement = (element, content, color) => {
     let elements = [...document.querySelectorAll(element)]
+    console.log("call UnderlineElement");
 
     elements.filter(el => el.textContent.includes(content)).forEach(el => {
       el.style.textDecoration = "underline"
@@ -54,9 +41,28 @@ function main() {
     })
   }
 
+  function waitForElm(selector) {
+    return new Promise(resolve => {
+      if (document.querySelector(selector)) {
+        return resolve(document.querySelector(selector))
+      }
+
+      const observer = new MutationObserver(mutations => {
+        if (document.querySelector(selector)) {
+          observer.disconnect()
+          resolve(document.querySelector(selector))
+        }
+      })
+
+      observer.observe(document.body, {
+        childList: true,
+        subtree: true
+      })
+    })
+  }
 
   waitForElm('.sc-15mtwvo-0').then(() => {
-    console.log('Element is loaded');
+    console.log('Element is loaded')
 
     ChangeElementColor(".sc-15mtwvo-0", mapColor)
 
@@ -64,27 +70,18 @@ function main() {
     UnderlineElement(".StyledButton-sc-qe3ace-0", "[Warning]", "#ffa500")
     UnderlineElement(".StyledButton-sc-qe3ace-0", "[Success]", "#00ff00")
 
-  });
+  })
 
+  document.onclick = function (event) {
+    waitForElm('.sc-15mtwvo-0').then(() => {
+      console.log('Element is loaded')
 
-  function waitForElm(selector) {
-    return new Promise(resolve => {
-      if (document.querySelector(selector)) {
-        return resolve(document.querySelector(selector));
-      }
+      ChangeElementColor(".sc-15mtwvo-0", mapColor)
 
-      const observer = new MutationObserver(mutations => {
-        if (document.querySelector(selector)) {
-          observer.disconnect();
-          resolve(document.querySelector(selector));
-        }
-      });
+      UnderlineElement(".StyledButton-sc-qe3ace-0", "[Failed]", "#ff0000")
+      UnderlineElement(".StyledButton-sc-qe3ace-0", "[Warning]", "#ffa500")
+      UnderlineElement(".StyledButton-sc-qe3ace-0", "[Success]", "#00ff00")
 
-      observer.observe(document.body, {
-        childList: true,
-        subtree: true
-      });
-    });
+    })
   }
-
 }
